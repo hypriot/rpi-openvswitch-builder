4facba2d49f167542ce69a2d283fb9903b9327c8
Dieter Reuter <dieter.reuter@me.com>
2015-03-29 21:47:41 +0200

    compile without running the tests

--- run-builder.sh
+++ run-builder.sh
@@ -2,4 +2,4 @@
 mkdir -p ./builds
 sudo modprobe openvswitch
 lsmod | grep openvswitch
-docker run --rm -ti --cap-add NET_ADMIN -v $(pwd)/builds:/builds hypriot/rpi-openvswitch-builder /bin/bash -c 'DEB_BUILD_OPTIONS="parallel=8" fakeroot debian/rules binary && cp /src/*.deb /builds/ && chmod a+rw /builds/*'
+docker run --rm -ti --cap-add NET_ADMIN -v $(pwd)/builds:/builds hypriot/rpi-openvswitch-builder /bin/bash -c 'DEB_BUILD_OPTIONS="parallel=8 nocheck" fakeroot debian/rules binary && cp /src/*.deb /builds/ && chmod a+rw /builds/*'
